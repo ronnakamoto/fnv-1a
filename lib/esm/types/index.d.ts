d9a0b91dc82d502ad8d972bcf3b54c0e97c0df6b
ronnakamoto <adarshron@Adarshs-MacBook-Air.local>
2023-02-03 07:24:31 +0530

    chore: updated gitignore

--- lib/esm/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Input, Options } from "./types";
-export default function fnv1a(data: Input, { size, outType }: Options): void;
-//# sourceMappingURL=index.d.ts.map
--- lib/esm/types/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Options {
-    size: 32 | 64 | 128 | 512 | 1024;
-    outType?: bigint | string;
-}
-export type Input = Record<any, any> | string | number;
-//# sourceMappingURL=types.d.ts.map
